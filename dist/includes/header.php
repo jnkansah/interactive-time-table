1fc316c6fad5f92f83bf87280284e7f0651f5e57
jnkansah <jaydankansah14@gmail.com>
2024-07-08 14:33:27 -0400

    adding dist

---
+++ dist/includes/header.php
@@ -0,0 +1,15 @@
+<?php
+if(empty($_SESSION['id'])):
+header('Location:../index.php');
+endif;
+date_default_timezone_set("Asia/Manila"); 
+
+if($_SESSION['type']=="admin")
+  include('../dist/includes/header_admin.php');
+else if ($_SESSION['type']=="student")
+  include('../dist/includes/header_admin.php');
+else
+  include('../dist/includes/header_faculty.php');
+?>
+
+
